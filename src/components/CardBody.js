c0f9137c716aaebb51f11662d5d5268877aea7b3
Tamika Dantes <chelley.tamz@gmail.com>
2022-01-20 12:16:27 -0500

    added more characters and sorted cards

--- src/components/CardBody.js
+++ src/components/CardBody.js
@@ -9,6 +9,12 @@ import Jayce from "../images/Jayce.jpeg";
 import Jinx from "../images/Jinx.jpeg";
 import Mel from "../images/Mel.jpeg";
 import Silco from "../images/Silco.png";
+import Caitlyn from "../images/Caitlyn.JPG";
+import Ekko from "../images/Ekko.JPG";
+import Heimerdinger from "../images/Heimerdinger.JPG";
+import Singed from "../images/Singed.JPG";
+import Vander from "../images/Vander.JPG";
+import Viktor from "../images/Viktor.jpg";
 
 function CardBody() {
     const char_info = [
@@ -22,7 +28,7 @@ function CardBody() {
           abilities: ["Brawler", "Leader", "Detective"]
         },
         {
-            name: "Jinx",
+            name: "Jinx (Powder)",
             avatar: ZaunCrest,
             image: Jinx,
             alt: "Jinx from Arcane",
@@ -56,11 +62,69 @@ function CardBody() {
             species: "Human",
             year_of_origin: "963 AN - 966 AN",
             abilities: ["Diplomacy", "Painting"]
+          },
+          {
+            name: "Caitlyn",
+            avatar: PiltoverCrest,
+            image: Caitlyn,
+            alt: "Caitlyn from Arcane",
+            species: "Human",
+            year_of_origin: "970 AN - 973 AN",
+            abilities: ["Detective"]
+          },
+          {
+            name: "Ekko",
+            avatar: ZaunCrest,
+            image: Ekko,
+            alt: "Ekko from Arcane",
+            species: "Human",
+            year_of_origin: "979 AN",
+            abilities: ["Brawler"]
+          },
+          {
+            name: "Heimerdinger",
+            avatar: PiltoverCrest,
+            image: Heimerdinger,
+            alt: "Heimerdinger from Arcane",
+            species: "Yordle",
+            year_of_origin: "Earlier than 680 AN",
+            abilities: ["Diplomacy", "Teacher"]
+          },
+          {
+            name: "Singed",
+            avatar: ZaunCrest,
+            image: Singed,
+            alt: "Singed from Arcane",
+            species: "Chemically Altered Human",
+            year_of_origin: "876 AN - 916 AN",
+            abilities: ["Inventor"]
+          },
+          {
+            name: "Vander",
+            avatar: ZaunCrest,
+            image: Vander,
+            alt: "Vander from Arcane",
+            species: "Human",
+            year_of_origin: "951 AN - 956 AN",
+            abilities: ["Brawler", "Barkeep", "Leader"]
+          },
+          {
+            name: "Viktor",
+            avatar: ZaunCrest,
+            image: Viktor,
+            alt: "Viktor from Arcane",
+            species: "Human Cyborg",
+            year_of_origin: "961 AN - 968 AN",
+            abilities: ["Inventor"]
           }
       ]
 
+    const sorted = char_info.sort((a, b) => {
+        return a.name > b.name ? 1 : ((b.name > a.name) ? -1 : 0)
+    });
+
     return (
-        char_info.map((info) => {
+        sorted.map((info) => {
             return (
                 <Card className='cardBody' sx={{
                     width: 290,
@@ -85,7 +149,7 @@ function CardBody() {
                     /> 
                 </Card>
             );
-        })    
+        })  
     );
 }
 
